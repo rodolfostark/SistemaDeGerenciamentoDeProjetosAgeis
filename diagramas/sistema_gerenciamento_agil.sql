65c3a716205367c4c62eb041555d35d9719b2ad8
Jonatas Rodolfo Pereira dos Santos <40967087+rodolfostark@users.noreply.github.com>
2019-11-25 11:12:30 -0300

    Atualizando o SQL do banco de dados, introduzi algumas variáveis a mais para facilitar a implementação.

--- diagramas/sistema_gerenciamento_agil.sql
+++ diagramas/sistema_gerenciamento_agil.sql
@@ -3,35 +3,41 @@ CREATE DATABASE sistema_gerenciamento_agil;
 USE sistema_gerenciamento_agil;
 
 CREATE TABLE task(
-	id_task INT NOT NULL,
+		id_task INT NOT NULL,
     descricao VARCHAR(250),
     prazo DATE,
     PRIMARY KEY(id_task)
 );
 
 CREATE TABLE programador(
-	id_programador INT NOT NULL,
-    nome VARCHAR(250),
+		id_programador INT NOT NULL,
+		nome_usuario VARCHAR(250) NOT NULL,
+		senha INT NOT NULL,
+		eh_lider BOOLEAN,
+    nome VARCHAR(250) NOT NULL,
     PRIMARY KEY (id_programador)
 );
 
 CREATE TABLE lider(
-	id_lider INT NOT NULL,
-    nome VARCHAR(250),
-     PRIMARY KEY(id_lider)
+		id_lider INT NOT NULL,
+		nome_usuario VARCHAR(250) NOT NULL,
+		eh_lider BOOLEAN,
+		senha INT NOT NULL,
+    nome VARCHAR(250) NOT NULL,
+    PRIMARY KEY(id_lider)
 );
 
 CREATE TABLE projeto(
 	id_projeto 	INT NOT NULL,
-    descricao VARCHAR(250),
-    data_cadastro DATE,
-    id_lider INT NOT NULL,
+  descricao VARCHAR(250) NOT NULL,
+  data_cadastro DATE,
+  id_lider INT NOT NULL,
 	PRIMARY KEY(id_projeto),
-    FOREIGN KEY (id_lider) REFERENCES lider(id_lider)
+  FOREIGN KEY (id_lider) REFERENCES lider(id_lider)
 );
 
 CREATE TABLE task_projeto(
-	id_task INT NOT NULL,
+		id_task INT NOT NULL,
     id_projeto INT NOT NULL,
     PRIMARY KEY(id_task, id_projeto),
     FOREIGN KEY (id_task) REFERENCES task(id_task),
@@ -39,7 +45,7 @@ CREATE TABLE task_projeto(
 );
 
 CREATE TABLE task_programador(
-	id_task INT NOT NULL,
+		id_task INT NOT NULL,
     id_programador INT NOT NULL,
     PRIMARY KEY (id_task, id_programador),
     FOREIGN KEY (id_task) REFERENCES task(id_task),
